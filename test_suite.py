6d5ba3ce949f0788a04049445fb9221d5791d670
SIJ82 <si-j@outlook.it>
2024-09-22 22:27:24 +0200

    catch

--- test_suite.py
+++ test_suite.py
@@ -32,43 +32,47 @@ dataset :list[dict[str, any]]= []
 
 
 for m in models:
-    model = WhisperModel(base_path+m, device="cpu", compute_type="int8")
-    print("\n------------------------")
-    print(f"Testing model {m}\n")
+    try:
+        model = WhisperModel(base_path+m, device="cpu", compute_type="int8")
+        print("\n------------------------")
+        print(f"Testing model {m}\n")
 
-    n_a=0
-    for a in audios:
-        start = time.time()
-        segments, info = model.transcribe(a, beam_size=5, language="it", vad_filter=True, without_timestamps=True) 
-        print(f"\tTranscribing : {a} - {info.duration}s")
-        
-        text = ""
-        for segment in segments:
-            text+=segment.text
-        
-        e_wer = wer(ref[n_a], text, transforms, transforms)
-        e_wil = wil(ref[n_a], text, transforms, transforms)
-        e_wip = wip(ref[n_a], text, transforms, transforms)
-        e_mer = mer(ref[n_a], text, transforms, transforms)
+        n_a=0
+        for a in audios:
+            start = time.time()
+            segments, info = model.transcribe(a, beam_size=5, language="it", vad_filter=True, without_timestamps=True) 
+            print(f"\tTranscribing : {a} - {info.duration}s")
+            
+            text = ""
+            for segment in segments:
+                text+=segment.text
+            
+            e_wer = wer(ref[n_a], text, transforms, transforms)
+            e_wil = wil(ref[n_a], text, transforms, transforms)
+            e_wip = wip(ref[n_a], text, transforms, transforms)
+            e_mer = mer(ref[n_a], text, transforms, transforms)
 
-        end = time.time() - start
-        #print(text)
-        print(f"\tDone in {end} - wer:{e_wer}\n")
+            end = time.time() - start
+            #print(text)
+            print(f"\tDone in {end} - wer:{e_wer}\n")
 
-        data = {
-            "model" : m,
-            "audio" : a,
-            "duration" : info.duration,
-            "duration_after_vad" : info.duration_after_vad,
-            "time" : end,
-            "wer" : e_wer,
-            "wil" : e_wil,
-            "wip" : e_wip,
-            "mer" : e_mer
-        }
+            data = {
+                "model" : m,
+                "audio" : a,
+                "duration" : info.duration,
+                "duration_after_vad" : info.duration_after_vad,
+                "time" : end,
+                "wer" : e_wer,
+                "wil" : e_wil,
+                "wip" : e_wip,
+                "mer" : e_mer
+            }
 
-        dataset.append(data)
-        n_a += 1
+            dataset.append(data)
+            n_a += 1
+    except:
+        dataset.append(  { "model": m, "audio": "aborted" }  )
+
 
 
 with open('./data/results.csv', 'w', newline='') as csvfile:
