3a6df3d5089b2dea3a823d79989578d88acd4705
SIJ82 <si-j@outlook.it>
2024-09-29 13:44:25 +0200

    suite update

--- test_suite.py
+++ test_suite.py
@@ -14,7 +14,7 @@ models = ["tiny", "base", "small", "medium", "large-v3"]
 dataset_path = "/models/it/"
 dataset = pd.read_csv(dataset_path+"validated.tsv", sep="\t")
 dataset=dataset.drop(columns=["client_id", "sentence_id", "sentence_domain", "up_votes", "down_votes", "age", "gender", "accents", "variant", "locale", "segment"])
-
+dataset = dataset.head(10)
 
 transforms = jiwer.Compose(
     [
@@ -27,63 +27,58 @@ transforms = jiwer.Compose(
     ]
 )
 
-ref = [
-    "Allora, questo è un test di registrazione, sto provando a parlare, ora seguiranno un paio di comandi, prima piano, poi veloce. Alexa, avvia una conversazione rapida. Alexa, avvia una conversazione rapida. Alexa, avvia una conversazione rapida. Alza il volume, abbassa il volume. Alza il volume, abbassa il volume. Alza il volume, abbassa il volume.",
-    "Questa è un'altra prova, barbabietola da zucchero. Vorrei fare un ordine. Ok, ciao"
-    ]
+def errors(data:dict,text:str,  ref:str):
+    data["wer"] = wer(ref, text, transforms, transforms)
+    data["wil"] = wil(ref, text, transforms, transforms)
+    data["wip"] = wip(ref, text, transforms, transforms)
+    data["mer"] = mer(ref, text, transforms, transforms)
+    return data
 
-dataset :list[dict[str, any]]= []
+def transcribe(model:WhisperModel, audio_path:str):
+    data={"model":"m"}
+    start = time.time()
+    #print("starting transcription...", dataset_path+"clips/"+audio_path)
 
+    segments, info = model.transcribe(dataset_path+"clips/"+audio_path, beam_size=5, language="it", vad_filter=True, without_timestamps=True)
+    text: str = ""
+    for segment in segments:
+        text+=segment.text
 
-for m in models:
-    try:
-        model = WhisperModel(base_path+m, device="cpu", compute_type="int8")
-        print("\n------------------------")
-        print(f"Testing model {m}\n")
+    end:float = time.time() - start
 
-        n_a=0
-        for a in audios:
-            start = time.time()
-            segments, info = model.transcribe(a, beam_size=5, language="it", vad_filter=True, without_timestamps=True) 
-            print(f"\tTranscribing : {a} - {info.duration}s")
-            
-            text = ""
-            for segment in segments:
-                text+=segment.text
-            
-            e_wer = wer(ref[n_a], text, transforms, transforms)
-            e_wil = wil(ref[n_a], text, transforms, transforms)
-            e_wip = wip(ref[n_a], text, transforms, transforms)
-            e_mer = mer(ref[n_a], text, transforms, transforms)
+    data["audio"] = audio_path
+    data["duration"] = info.duration
+    data["duration_after_vad"] = info.duration_after_vad
+    data["time"] = end
 
-            end = time.time() - start
-            #print(text)
-            print(f"\tDone in {end} - wer:{e_wer}\n")
-
-            data = {
-                "model" : m,
-                "audio" : a,
-                "duration" : info.duration,
-                "duration_after_vad" : info.duration_after_vad,
-                "time" : end,
-                "wer" : e_wer,
-                "wil" : e_wil,
-                "wip" : e_wip,
-                "mer" : e_mer
-            }
-
-            dataset.append(data)
-            n_a += 1
-    except:
-        dataset.append(  { "model": m, "audio": "aborted" }  )
+    return (data, text)
 
 
 
 with open('./data/results.csv', 'w', newline='') as csvfile:
-    fieldnames = [key for key in dataset[0]]
-    print(fieldnames)
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer = csv.DictWriter(csvfile, 
+        fieldnames=["model","audio", "duration", "duration_after_vad", "time", "wer", "wil", "wip", "mer"])
     writer.writeheader()
-    writer.writerows(dataset)
+    
+    for m in models:
+        try:
+            model = WhisperModel(base_path+m, device="cpu", compute_type="int8")
+            print("\n------------------------")
+            print(f"Testing model {m}\n")
+
+            data={}
+            for i in range(len(dataset)):
+                try:
+                    print(f"Audio {i}/{len(dataset)}- {dataset.iloc[i]["path"]}")
+                    (data,text) = transcribe(model, dataset.iloc[i]["path"])
+                except:
+                    print(f"Audion n.{i} failed")
+                
+                data = errors(data, text,  dataset.iloc[i]["sentence"])
+                data["model"] = m
+                writer.writerow(data)
+
+        except:
+            pass
     
 
